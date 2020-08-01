8e1a4d0da8e4b5397d799bc4b08d11b0ddb9023a
Christine Yang <christine.yang@yale.edu>
2020-08-01 16:33:55 -0400

    join rooms

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -3,9 +3,41 @@ import { Link } from "react-router-dom";
 
 import './Join.css';
 
+//Update state to avoid double booking rooms 
+//Create a App state
+
+
+// function genRoom() {
+//   var roomList = ["a", "b", "c"]
+//   return roomList[Math.floor(Math.random() * roomList.length)]
+// }
+
 export default function SignIn() {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [usedRooms, setUsedRooms] = useState([]);
+  var roomList = ["a", "b", "c"];
+
+  const genRoom = () => {
+    var bool1 = true;
+    var count = 0;
+    while(bool1 && count < 10) {
+      var chosenRoom = roomList[Math.floor(Math.random() * roomList.length)];
+
+      console.log(!usedRooms.includes(chosenRoom))
+      console.log("CHosen:" + chosenRoom)
+      console.log("Used" + usedRooms)
+      
+      if (!usedRooms.includes(chosenRoom)) {
+        console.log("I made it!")
+        //usedRooms.push(chosenRoom);
+        setUsedRooms([...usedRooms, chosenRoom]);
+        setRoom(chosenRoom);
+        bool1 = false;
+      }
+      count++;
+    }
+  }
 
   return (
     <div className="joinOuterContainer">
@@ -14,13 +46,17 @@ export default function SignIn() {
         <div>
           <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
         </div>
-        <div>
-          <input placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)} />
-        </div>
-        <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+
+        <Link onClick={genRoom} to={`/chat?name=${name}&room=${room}`}>
           <button className={'button mt-20'} type="submit">Sign In</button>
         </Link>
+
+        
       </div>
     </div>
   );
 }
+
+
+//genRoom(roomList)
+//e => (!name || !room) ? e.preventDefault() : null
