57bb5da9e619495332efcab11231b08592f7f8ac
Revanth Tiruveedhi <37131393+trevanth24@users.noreply.github.com>
2021-01-07 02:31:20 -0500

    Lol All edits I have made
    I added type casting to the Join and Lobby page. Futhermore, changed some of the wording and other input methods for Lobby items. Futher CSS needs to be done for the errors that show for typecasting but that will need to be a new ticket

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import React, { useReducer, useState } from 'react';
+import { Link, useHistory } from "react-router-dom";
 import iro from '@jaames/iro';
 
 
@@ -27,18 +27,112 @@ export default function Join() {
   const [name, setName] = useState('');
   const [color, setColor] = useState('');
   const [room, setRoom] = useState('');
+  const [fieldError, setFieldError] = useState({nameError:'', roomError:''});
+
+  let history = useHistory();
 
   colorPicker.on('color:change', function(color) {
   // log the current color as a HEX string
   setColor(color.hexString);
-  console.log(color.hexString);
 });
   
-  function handleUserInput(e, name){
+  function handleUserInput(e, scrollName){   
     if (e.key === 'Enter') {
-        scrollTo(name);
+      let nameBool = validateName();
+      if (nameBool===true){
+        scrollTo(scrollName)
+      }
+      else{
+        scrollTo("name")
+      }
+      
     }
   }
+  
+  function handleNameButton(event) {
+    //handler for button click
+    event.preventDefault();
+    let formBool = validateName();
+    if (formBool === true) {
+      scrollTo("colpick");
+    }
+    else {
+      scrollTo("name");
+    }
+  }
+
+  function validateName() {
+    //Checking if the name is not empty
+    if (name === ''){
+      setFieldError(fieldError => ({...fieldError, nameError: "Name Field cannot be empty!"}));
+      return false;
+    }
+    //If does not follow the right pattern
+    else if (!name.match(/^[0-9a-zA-Z]+$/)) {
+      setFieldError(fieldError => ({...fieldError, nameError: "Only Letters"}));
+      return false;
+   }
+    //Set item is right
+    else {
+      setFieldError(fieldError => ({...fieldError, nameError: ""}));
+      return true;
+    }
+  }
+
+  function validateRoomKey(scrollName) {
+    //Checking if the name is not empty
+    if (room === ''){
+      setFieldError(fieldError => ({...fieldError, roomError: "secret word cannot be empty!"}));
+      return false;
+    }
+    //If does not follow the right pattern
+    else if (!room.match(/^[a-zA-Z]+$/)) {
+      setFieldError(fieldError => ({...fieldError, roomError: "only Letters in the secret word"}));
+      return false;
+   }
+    //Set item is right
+    else {
+      setFieldError(fieldError => ({...fieldError, roomError: ""}));
+      return true;
+    }
+
+  }
+
+  function handleNewRoomSubmit (event) {
+    event.preventDefault();
+    let nameBool = validateName();
+    if (nameBool === true) {
+      history.push({
+        pathname: '/setup', 
+        state: { name, color }
+      }); 
+    }
+    else {
+      scrollTo("name");
+    }
+  }
+
+  function handleJoinRoomSubmit(event){
+    event.preventDefault();
+    let nameBool = validateName();
+    if (nameBool === true) {
+      let roomBool = validateRoomKey();
+      if (roomBool === true) {
+        history.push({
+          pathname: '/lobby', 
+          state: { name, color, room }
+        }); 
+      }
+      else {
+        scrollTo("secretword");
+      } 
+    }
+    else {
+      scrollTo("name");
+    }
+  }
+
+
 
   function scrollTo(name)
   {
@@ -56,7 +150,7 @@ export default function Join() {
       <div id="aboutUs">hello lorem ipsum</div>
       <Link to={`/about`}><div className="infoCircle">i</div></Link>
         <section id="welcome">
-        <h1>welcome to</h1>
+        <h1>Welcome to</h1>
         <h1 id="title">Campfire</h1>
           <div className="welcomebg">
           </div>
@@ -66,9 +160,10 @@ export default function Join() {
         <section id="name">
           <h1 className="heading">what is your name?</h1>
           <div>
+            <p color="red">{fieldError.nameError}</p>
             <input placeholder="Name" tabIndex = "0" className="joinInput" type="text" onChange={(event) => {
               setName(event.target.value)}} onKeyPress={(e) => {handleUserInput(e, 'colpick')}} />
-            <a href="#colpick"><button className={'button'} type="submit" tabIndex = "1">continue</button></a>
+            <button className={'button'} type="submit" tabIndex = "1" onClick={handleNameButton}>continue</button>
           </div>
         </section>
 
@@ -83,9 +178,9 @@ export default function Join() {
         <section id="choice">
           <div className="bubblecontain">
           {/* <Link to={`/setup?name=${name}color=${color}`}> */}
-          <Link to={ {pathname: '/setup', state: { name, color }}}>
-            <button className={'button'} type="submit" tabIndex = "4">start a campfire</button>
-          </Link>
+          {/* <Link to={ {pathname: '/setup', state: { name, color }}}> */}
+          <button className={'button'} type="submit" tabIndex = "4" onClick={handleNewRoomSubmit}>start a campfire</button>
+          {/* </Link> */}
           <button className={'button'} type="submit" onClick={revealScroll} tabIndex="5">join a campfire</button>
         </div>
         </section>
@@ -93,6 +188,7 @@ export default function Join() {
         <section id="secretword" className="reveal-if-active">
           <h1 className="heading">what is the secret word</h1>
           <div>
+          <p color="red">{fieldError.roomError}</p>
             <input placeholder="Name" className="joinInput" type="text"  tabIndex = "4" 
               onChange={(event) => {setRoom(event.target.value)}} 
               // onKeyPress={(e) => {
@@ -103,9 +199,9 @@ export default function Join() {
               //}
             />
           </div>
-          <Link to={ {pathname: '/lobby', state: { name, color, room }}}>
-            <button className={'button mt-20'} type="submit"> Continue </button>
-          </Link>
+          
+            <button className={'button mt-20'} type="submit" onClick = {handleJoinRoomSubmit}> Continue </button>
+    
         </section>
       </div>
   );
--- client/src/components/Setup/Setup.js
+++ client/src/components/Setup/Setup.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import queryString from 'query-string';
-import { Link } from "react-router-dom";
+// import queryString from 'query-string';
+import { useHistory } from "react-router-dom";
+import InputRange from 'react-input-range';
+import "react-input-range/lib/css/index.css";
 //import { Lobby } from "../Lobby/Lobby.js";
 
 import './Setup.css';
@@ -17,11 +19,15 @@ const Setup = ({ location }) => {
   const [usedRooms, setUsedRooms] = useState([]);
   const [mode, setMode] = useState('');
   const [topic, setTopic] = useState('');
-  const [lowerwordlimit, setLowerWordLimit] = useState('');
-  const [higherwordlimit, setHigherWordLimit] = useState('');
-  const [storylength, setStoryLength] = useState('');
-  const [timelimit, setTimeLimit] = useState('');
+  const [lowerwordlimit, setLowerWordLimit] = useState('20');
+  const [higherwordlimit, setHigherWordLimit] = useState('100');
+  const [storylength, setStoryLength] = useState(3);
+  const [timelimit, setTimeLimit] = useState(60);
+  const [fieldError, setFieldError] = useState({});
+  // const [testValue, setTestValue] = useState({value: { min: 20, max: 100 }})
+
   const ENDPOINT = 'https://campfire-storytellers.herokuapp.com/';
+  let history = useHistory();
   
   var adj_arr = ['aggressive', 'alert', 'alive', 'ancient', 'anxious', 'arrow', 'attractive', 'average', 'bad', 'beautiful', 'beige', 'better', 'big', 'bitter', 'black', 'blue', 'brown', 'bumpy', 'busy', 'careful', 'cheap', 'chestnut', 'clear', 'cold', 'combative', 'cool', 'cotton', 'crazy', 'crooked', 'crystal', 'dangerous', 'dead', 'delicious', 'dim', 'drab', 'dry', 'dull', 'dusty', 'elderly', 'excited', 'expensive', 'fancy', 'fat', 'few', 'filthy', 'fresh', 'fuzzy', 'giant', 'good', 'graceful', 'granite', 'green', 'handsome', 'happy', 'hard', 'harsh', 'hollow', 'hot', 'huge', 'hungry', 'large', 'lazy', 'light', 'long', 'low', 'massive', 'mellow', 'melodic', 'miniscule', 'modern', 'new', 'noisy', 'oak', 'octagonal', 'old', 'orange', 'oval', 'petite', 'pink', 'plain', 'plastic', 'poor', 'puny', 'purple', 'quiet', 'rainy', 'red', 'rich', 'right', 'round', 'sad', 'safe', 'salty', 'sane', 'scared', 'shallow', 'sharp', 'shiny', 'short', 'shrill', 'shy', 'skinny', 'small', 'soft', 'solid', 'sore', 'sour', 'square', 'steep', 'sticky', 'strong', 'superior', 'sweet', 'swift', 'tan', 'tart', 'teak', 'teeny', 'terrible', 'tiny', 'tired', 'tremendous', 'triangular', 'ugly', 'unusual', 'weak', 'weary', 'wet', 'whispering', 'white', 'wild', 'wooden', 'woolen', 'wrong', 'yellow', 'young']
   var noun_arr = ['Adventure', 'Animals', 'backpack', 'boots', 'cabin', 'camp', 'camper', 'campfire', 'campground', 'canoe', 'canteen', 'cap', 'caravan', 'climb', 'compass', 'equipment', 'evergreen', 'fishing', 'flashlight', 'forest', 'hammock', 'hat', 'hike', 'hiking', 'boots', 'hunting', 'hut', 'kayak', 'knapsack', 'lake', 'lantern', 'map', 'moon', 'natural', 'nature', 'oar', 'outdoors', 'outside', 'paddle', 'park', 'path', 'tent', 'rope', 'scenery', 'sleepingBag', 'stars', 'statePark', 'sun', 'sunscreen', 'tarp', 'tent', 'trail', 'trailMix', 'trailer', 'trees', 'trip', 'vest', 'walking', 'waterBottle', 'waterfall', 'wildlife', 'woods', 'Backpack', 'Binoculars', 'Boots', 'Marshmallow', 'Radio', 'SleepingBag', 'Tent', 'Torch', 'WaterBottle', 'Canopy', 'Forest', 'Mushroom', 'Terrain', 'Wildlife', 'Birdwatching', 'Hike', 'Stargazing', 'Scouting']
@@ -46,7 +52,7 @@ const Setup = ({ location }) => {
       console.log("Used" + usedRooms)
       
       if (!usedRooms.includes(chosenRoom)) {
-        console.log("I made it!")
+        // console.log("I made it!")
         setUsedRooms([...usedRooms, chosenRoom]);
         setRoom(chosenRoom);
         bool1 = false;
@@ -56,7 +62,6 @@ const Setup = ({ location }) => {
   }
 
   useEffect(() => {
-    // const { name } = queryString.parse(location.search);
     const {name, color} =  location.state;
 
     genRoom(roomList);
@@ -64,6 +69,68 @@ const Setup = ({ location }) => {
     setName(name);
 
   }, [ENDPOINT, location.search]);
+
+  function handleSetupSubmit (event) {
+    event.preventDefault();
+    let boolForms = validateFields();
+    if (boolForms == true) {
+      //pass to next level
+      history.push(
+      {
+        pathname: '/lobby',
+        state: {name, color, room, mode, topic, lowerwordlimit, higherwordlimit, storylength,timelimit, isHost: true}
+      });
+    }
+  }
+
+  function validateFields () {
+    let internal_bool = true;
+
+    if (topic === ''){
+      setFieldError(fieldError => ({...fieldError, topicError: "Topic cannot be empty! Its fun to have one :)"}));
+      internal_bool = false;
+    }
+    else {
+      setFieldError(fieldError => ({...fieldError, topicError: ""}));
+    }
+
+    //Check if charachters are only numbers
+    if (lowerwordlimit !== "" && !(/^\d+$/.test(lowerwordlimit))) {
+      setFieldError(fieldError => ({...fieldError, lwlError: "min words must be a positive number!"}));
+      internal_bool = false;
+    }
+    else if (lowerwordlimit !== "" && (/^\d+$/.test(lowerwordlimit))){
+      //Check if number is the right range
+      if(parseInt(lowerwordlimit) < 0 || parseInt(lowerwordlimit) > 9999) {
+        setFieldError(fieldError => ({...fieldError, lwlError: "min words is out of range! Pick a number 0-9999"}));
+        internal_bool = false;
+      }
+      else if (higherwordlimit !== "" && (/^\d+$/.test(higherwordlimit)) && !(parseInt(higherwordlimit) < 0 || parseInt(higherwordlimit) > 9999)) {
+        // Checking Min Max relation
+        if(parseInt(higherwordlimit) <= parseInt(lowerwordlimit)){
+          setFieldError(fieldError => ({...fieldError, lwlError: "min words must be smaller than max words!", hwlError: "max words must be larger than min words!"}));
+          internal_bool = false;
+        }
+        else {
+          setFieldError(fieldError => ({...fieldError, lwlError: "", hwlError: ""}));
+        }
+      }
+    }
+    
+    //Check if charachters are only numbers
+    if (higherwordlimit !== "" && !(/^\d+$/.test(higherwordlimit))){
+      setFieldError(fieldError => ({...fieldError, hwlError: "max words must be a number!"}));
+      internal_bool = false;
+    }
+    else if (higherwordlimit !== "" && (/^\d+$/.test(higherwordlimit))){
+      //Check if number is the right range
+      if(parseInt(higherwordlimit) < 0 || parseInt(higherwordlimit) > 9999) {
+        setFieldError(fieldError => ({...fieldError, hwlError: "max words is out of range! Pick a number 0-9999"}));
+        internal_bool = false;
+      }
+    }
+    return internal_bool;
+  }
   
 
 
@@ -72,36 +139,76 @@ const Setup = ({ location }) => {
         <section id = "general">
           <h1> set your preferences </h1> <br></br>
             <label for="topic">Topic:</label>
+            <p color="red">{fieldError.topicError}</p>
             <input type="text" id="topic" name="topic" onChange={(event) => {
               setTopic(event.target.value)}}></input>
             <div className="setup-grid">
               <div>
-                <label for="lowerwordlimit">Lower Word Limit:</label>
+              <label for="storylength">Number of Rounds:</label>
+              <select name="storylength" id="storylength"value={storylength} onChange={(event) => {setStoryLength(event.target.value)}}>
+                <option name="2">2</option>
+                <option name="3">3</option>
+                <option name="4">4</option>
+                <option name="5">5</option>
+                <option name="6">6</option>
+                <option name="7">7</option>
+                <option name="8">8</option>
+                <option name="9">9</option>
+                <option name="10">10</option>
+              </select>
+              </div>
+
+              <div>
+                <label for="timelimit">Time Limit (sec):</label>
+                <select id="timelimit" name="timelimit" value={timelimit} onChange={(event) => {setTimeLimit(event.target.value)}}>
+                  <option name="30">30</option>
+                  <option name="40">40</option>
+                  <option name="50">50</option>
+                  <option name="60">60</option>
+                  <option name="70">70</option>
+                  <option name="80">80</option>
+                  <option name="90">90</option>
+                  <option name="100">100</option>
+                  <option name="110">110</option>
+                  <option name="120">120</option>
+                  <option name="130">130</option>
+                  <option name="100">140</option>
+                  <option name="110">150</option>
+                  <option name="120">160</option>
+                  <option name="130">170</option>
+                  <option name="130">180</option>
+                  <option name="none">Why Have a Time Limit :)</option>
+                </select>
+              </div>
+
+              <div>
+                <label for="lowerwordlimit">Min Word Limit:</label>
+                <p color="red">{fieldError.lwlError}</p>
                 <input type="text" id="lowerwordlimit" name="lowerwordlimit" onChange={(event) => {
                   setLowerWordLimit(event.target.value)}}></input>
               </div>
               <div>
-                <label for="higherwordlimit">Higher Word Limit:</label>
+                <label for="higherwordlimit">Max Word Limit:</label>
+                <p color="red">{fieldError.hwlError}</p>
                 <input type="text" id="higherwordlimit" name="higherwordlimit" onChange={(event) => {
                   setHigherWordLimit(event.target.value)}}></input>
               </div>
-              <div>
-                <label for="storylength">Story Length:</label>
-                <input type="text" id="storylength" name="storylength" onChange={(event) => {
-                  setStoryLength(event.target.value)}}></input>
-              </div>
-              <div>
-                <label for="timelimit">Time Limit:</label>
-                <input type="text" id="timelimit" name="timelimit" onChange={(event) => {
-                  setTimeLimit(event.target.value)}}></input>
-              </div>
-              </div>
-        <Link to={ {pathname: '/lobby',
-          state: {name, color, room, mode, topic, lowerwordlimit, higherwordlimit, storylength,timelimit, isHost: true}}}>
-          <button className={'setup-button mt-20'} type="submit">Continue</button>
-        </Link>
 
-        
+              
+              {/* DISCUSS ON JUST USING A SLIDER */}
+              {/* <div>
+                <label for="higherwordlimit">Max Word Limit:</label>
+                <InputRange
+                  maxValue={300}
+                  minValue={0}
+                  value = {testValue.value}
+                  onChange={(value) => {setTestValue({ value }); setHigherWordLimit(toString(testValue.value.max));}} />
+              </div> */}
+
+              </div>
+          <div>
+            <button className={'setup-button mt-20'} type="submit" onClick = {handleSetupSubmit}>Continue</button>
+          </div>
         </section>
       </div>
   );
